69a62bcf284dcd8e083a8e4153e39830d6ad0368
Etherial <typicaluserinfinite@gmail.com>
2025-02-05 14:26:42 +0900

    remove enum from schema

--- schema.py
+++ schema.py
@@ -1,17 +1,9 @@
 from pydantic import BaseModel
-from enum import Enum, auto
-
-
-class ErrorCorrection(Enum):
-    ERROR_CORRECT_M = 0
-    ERROR_CORRECT_L = auto()
-    ERROR_CORRECT_H = auto()
-    ERROR_CORRECT_Q = auto()
 
 
 class QRCodeRequest(BaseModel):
     data: str
-    error_correction: ErrorCorrection = ErrorCorrection.ERROR_CORRECT_M
+    error_correction: int = 0
     version: int = 1
     box_size: int = 10
     border: int = 1
